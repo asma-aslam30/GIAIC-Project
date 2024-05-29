5dae036fcc9fc04f1e9f4824190ace8ce0598d84
asma-aslam30 <aslamasma986@gmail.com>
2024-05-29 19:31:24 +0500

    new

--- Number Guessing Game/index.js
+++ Number Guessing Game/index.js
@@ -1,18 +1,19 @@
 import inquirer from "inquirer";
-// 1.   computer will generate a random number
-// 2.   userinput for guessing number
-// 3.   campare userinput with computer generated numberand show result
 const randomNumber = Math.floor(Math.random() * 100);
 console.log(randomNumber);
-const answer = await inquirer.prompt([{
-        name: "userGuessNumber",
-        type: "number",
-        message: "Please! Guess a number between 1-100",
-    }]);
-console.log(answer);
-if (answer.userGuessNumber === randomNumber) {
-    console.log("Congratulations! you guessed a right number");
-}
-else {
-    console.log("Sorry! you guessed wrong number");
-}
+const guessNumber = async () => {
+    const answer = await inquirer.prompt([{
+            name: "userGuessNumber",
+            type: "number",
+            message: "Please guess a number between 1-100:",
+        }]);
+    console.log(answer);
+    if (answer.userGuessNumber === randomNumber) {
+        console.log("Congratulations! You guessed the right number.");
+    }
+    else {
+        console.log("Sorry! That's not the correct number. Try again.");
+        await guessNumber(); // Recursive call until the correct number is guessed
+    }
+};
+guessNumber();
--- Number Guessing Game/index.ts
+++ Number Guessing Game/index.ts
@@ -1,23 +1,22 @@
-
 import inquirer from "inquirer";
-// 1.   computer will generate a random number
-// 2.   userinput for guessing number
-// 3.   campare userinput with computer generated numberand show result
-const randomNumber = Math.floor(Math.random()*100);
+
+const randomNumber: number = Math.floor(Math.random() * 100);
 console.log(randomNumber);
 
-const answer = await inquirer.prompt([{
-    name:"userGuessNumber",
-    type:"number",
-    message:"Please! Guess a number between 1-100",
-}]);
-console.log(answer);
+const guessNumber = async (): Promise<void> => {
+    const answer = await inquirer.prompt([{
+        name: "userGuessNumber",
+        type: "number",
+        message: "Please guess a number between 1-100:",
+    }]);
+    console.log(answer);
 
-if(answer.userGuessNumber === randomNumber){
-console.log("Congratulations! you guessed a right number");
-}
-else{
-    console.log("Sorry! you guessed wrong number");
-    
-}
+    if (answer.userGuessNumber === randomNumber) {
+        console.log("Congratulations! You guessed the right number.");
+    } else {
+        console.log("Sorry! That's not the correct number. Try again.");
+        await guessNumber(); // Recursive call until the correct number is guessed
+    }
+};
 
+guessNumber();
